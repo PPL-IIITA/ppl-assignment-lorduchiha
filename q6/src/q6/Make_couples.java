2abc4889f1ac96ab7e368e16699f73073a244e76
NITESH SINGH MUNDA <munda392@gmail.com>
2017-04-10 07:24:39 +0530

    Update Make_couples.java

--- q6/src/q6/Make_couples.java
+++ q6/src/q6/Make_couples.java
@@ -3,11 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package q6;
-
-
-
-
 public class Make_couples {
     public void make_couples(Boys[] b,Girls[] g){
         for(int i=0;i<50;i++){
