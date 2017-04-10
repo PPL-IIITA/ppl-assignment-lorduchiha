c3eded31c1cf3e9765eeac7e20d9259eaf7d1d27
NITESH SINGH MUNDA <munda392@gmail.com>
2017-04-10 07:32:16 +0530

    Update Boys.java

--- q9/src/q9/Boys.java
+++ q9/src/q9/Boys.java
@@ -3,10 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package q9;
-
-
-
 public abstract class Boys {
     String name;
     int attractiveness;
