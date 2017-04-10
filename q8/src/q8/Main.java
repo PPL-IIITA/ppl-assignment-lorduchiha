a330b5f4d52ef91a27cd539a5a6d3fc008e346bc
NITESH SINGH MUNDA <munda392@gmail.com>
2017-04-10 07:31:13 +0530

    Update Main.java

--- q8/src/q8/Main.java
+++ q8/src/q8/Main.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package q8;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
