aed68ce6c1d5077b2bef6beef1352ed27de5fe36
NITESH SINGH MUNDA <munda392@gmail.com>
2017-04-10 07:40:07 +0530

    Update Logger.java

--- q10/src/q10/Logger.java
+++ q10/src/q10/Logger.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package q10;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
